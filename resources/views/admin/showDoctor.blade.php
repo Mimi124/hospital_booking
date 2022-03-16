0d5ad690880bf968e60b89bce63f59f6bc16f808
SamAllotey <samallotey7@gmail.com>
2022-03-16 00:18:49 +0000

    Update showDoctor.blade.php
    button to add new doctors

--- resources/views/admin/showDoctor.blade.php
+++ resources/views/admin/showDoctor.blade.php
@@ -17,10 +17,14 @@
  
         <!-- partial -->
 
+        
+
         <div class="container-fluid page-body-wrapper">
+          
 
             <div class="container" style="padding:100px;">
-    
+              <a class="btn btn-info" id="button" href="/add_doctor_view">Add New Doctor</a>
+              <br><br>
                 <table class="table table-striped table-primary">
                     <thead>
                       <tr>
@@ -44,10 +48,10 @@
                       <td>{{ $doctors->room }}</td>
                       <td><img height="100" width="100" src="{{ $doctors->image }}"</td>
                        <td>
-                          <a class="btn btn-danger" href="{{url('updateDoctor',$doctors->id)}}">Update</a>
+                          <a class="btn btn-primary" href="{{url('updateDoctor',$doctors->id)}}">Update</a>
                         
                       </td>  <td>
-                        <a onclick="return confirm('Are you sure you want to delete?')" class="btn btn-primary" href="{{url('deleteDoctor',$doctors->id)}}">Delete</a>
+                        <a onclick="return confirm('Are you sure you want to delete?')" class="btn btn-danger" href="{{url('deleteDoctor',$doctors->id)}}">Delete</a>
                       
                     </td>
 
