50e98911f36857f1b71659e74ac7139bcb820648
Mimi124 <miriamamoako98@gmail.com>
2022-04-03 10:41:31 +0000

    button updated

--- resources/views/user/appointment.blade.php
+++ resources/views/user/appointment.blade.php
@@ -11,43 +11,42 @@
         </ul>
     </div>
 @endif
-<div class="page-section">
-    <div class="container">
-      <h1 class="text-center wow fadeInUp">Make an Appointment</h1>
+<div class="page-section" id="make-appointment">
+  <div class="container">
+    <h1 class="text-center wow fadeInUp">Make an Appointment</h1>
 
-      <form class="main-form" action="{{url('appointment')}}" method="post" >
-        @csrf
-        <div class="row mt-5 ">
-          <div class="col-12 col-sm-6 py-2 wow fadeInLeft">
-            <input type="hidden" class="form-control" >
-          </div>
-          <div class="col-12 col-sm-6 py-2 wow fadeInRight">
-            <input type="hidden" class="form-control" name="user_id" >
-          </div>
-          <div class="col-12 col-sm-6 py-2 wow fadeInLeft" data-wow-delay="300ms">
-            <input type="date" name="date" class="form-control" required>
-          </div>
-          <div class="col-12 col-sm-6 py-2 wow fadeInRight" data-wow-delay="300ms">
-            <select name="doctor_id" id="doctor_id"  class="custom-select" required>
-              <option>--Select Doctor--</option>
-              @foreach($doctor as $doctors)
-              <option value="{{$doctors->id}}" class="form-control">{{$doctors->name}} | {{$doctors->speciality}}</option>
-              @endforeach
-            </select>
-          </div>
-          <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
-            <input type="text" class="form-control" name="number" placeholder="Number.." required>
-          </div>
-          <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
-            <textarea name="message" id="message"name="message" class="form-control" rows="6" placeholder="Enter message.." required></textarea>
-          </div>
+    <form class="main-form" action="{{url('appointment')}}" method="post" enctype="multipart/form">
+      @csrf
+      <div class="row mt-5 ">
+        <div class="col-12 col-sm-6 py-2 wow fadeInLeft">
+          <input type="hidden" class="form-control" >
         </div>
+        <div class="col-12 col-sm-6 py-2 wow fadeInRight">
+          <input type="hidden" class="form-control" name="user_id" >
+        </div>
+        <div class="col-12 col-sm-6 py-2 wow fadeInLeft" data-wow-delay="300ms">
+          <input type="date" name="date" class="form-control" required>
+        </div>
+        <div class="col-12 col-sm-6 py-2 wow fadeInRight" data-wow-delay="300ms">
+          <select name="doctor_id" id="doctor_id"  class="custom-select" required>
+            <option>--Select Doctor--</option>
+            @foreach($doctor as $doctors)
+            <option value="{{$doctors->id}}" class="form-control">{{$doctors->name}} | {{$doctors->speciality}}</option>
+            @endforeach
+          </select>
+        </div>
+        <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
+          <input type="text" class="form-control" name="number" placeholder="Number.." required>
+        </div>
+        <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
+          <textarea name="message" id="message"name="message" class="form-control" rows="6" placeholder="Enter message.." required></textarea>
+        </div>
+      </div>
 
-        {{-- <button type="submit" class="btn btn-primary mt-3 wow zoomIn">Submit Request</button> --}}
-        <button class="btn btn-outline-dark" type="submit">Submit Request</button>
+      {{-- <button type="submit" class="btn btn-primary mt-3 wow zoomIn">Submit Request</button> --}}
+      <button class="btn btn-outline-dark" type="submit">Submit Request</button>
 
-      </form>
-    </div>
-  </div> <!-- .page-section --> 
-  
-  {{-- value="{{$doctor->id}} --}}
+    </form>
+  </div>
+</div> <!-- .page-section --> 
+
--- resources/views/user/body.blade.php
+++ resources/views/user/body.blade.php
@@ -1,42 +1,51 @@
 <div class="page-hero bg-image overlay-dark" style="background-image: url(../assets/img/bg_image_1.jpg);">
-    <div class="hero-section">
-      <div class="container text-center wow zoomIn">
-        <span class="subhead">Let's make your life happier</span>
-        <h1 class="display-4">Healthy Living</h1>
-        <a href="/appointment" class="btn btn-primary">Book an Appointment</a>
-      </div>
+  <div class="hero-section">
+    <div class="container text-center wow zoomIn">
+      <span class="subhead">Let's make your life happier</span>
+      <h1 class="display-4">Healthy Living</h1>
+      <a class="btn btn-primary" id="book-appointment">Let's Consult</a>
     </div>
   </div>
+</div>
 
 
-  <div class="bg-light">
-    <div class="page-section py-3 mt-md-n5 custom-index">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-4 py-3 py-md-0">
-            <div class="card-service wow fadeInUp">
-              <div class="circle-shape bg-secondary text-white">
-                <span class="mai-chatbubbles-outline"></span>
-              </div>
-              <p><span>Chat</span> with a doctors</p>
+<div class="bg-light">
+  <div class="page-section py-3 mt-md-n5 custom-index">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-4 py-3 py-md-0">
+          <div class="card-service wow fadeInUp">
+            <div class="circle-shape bg-secondary text-white">
+              <span class="mai-chatbubbles-outline"></span>
             </div>
+            <p><span>Chat</span> with a doctors</p>
           </div>
-          <div class="col-md-4 py-3 py-md-0">
-            <div class="card-service wow fadeInUp">
-              <div class="circle-shape bg-primary text-white">
-                <span class="mai-shield-checkmark"></span>
-              </div>
-              <p><span>One</span>-Health Protection</p>
+        </div>
+        <div class="col-md-4 py-3 py-md-0">
+          <div class="card-service wow fadeInUp">
+            <div class="circle-shape bg-primary text-white">
+              <span class="mai-shield-checkmark"></span>
             </div>
+            <p><span>One</span>-Health Protection</p>
           </div>
-          <div class="col-md-4 py-3 py-md-0">
-            <div class="card-service wow fadeInUp">
-              <div class="circle-shape bg-accent text-white">
-                <span class="mai-basket"></span>
-              </div>
-              <p><span>One</span>-Health Pharmacy</p>
+        </div>
+        <div class="col-md-4 py-3 py-md-0">
+          <div class="card-service wow fadeInUp">
+            <div class="circle-shape bg-accent text-white">
+              <span class="mai-basket"></span>
             </div>
+            <p><span>One</span>-Health Pharmacy</p>
           </div>
         </div>
       </div>
     </div>
+  </div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script>
+$("#book-appointment").click(function() {
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#make-appointment").offset().top
+  }, 2000);
+});
+</script>
