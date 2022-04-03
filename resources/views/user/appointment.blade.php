744ceb8d0c6824a9275de36e1c2b52025a44e86c
Mimi124 <miriamamoako98@gmail.com>
2022-04-03 10:25:33 +0000

    updated

--- resources/views/user/appointment.blade.php
+++ resources/views/user/appointment.blade.php
@@ -15,7 +15,7 @@
     <div class="container">
       <h1 class="text-center wow fadeInUp">Make an Appointment</h1>
 
-      <form class="main-form" action="{{url('appointment')}}" method="post" enctype="multipart/form">
+      <form class="main-form" action="{{url('appointment')}}" method="post" >
         @csrf
         <div class="row mt-5 ">
           <div class="col-12 col-sm-6 py-2 wow fadeInLeft">
